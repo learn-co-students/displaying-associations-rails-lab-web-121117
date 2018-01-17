ce2df80661da0795cb6ba21df3627eacadfe3792
github username <github email address>
2018-01-17 15:05:42 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,11 @@
 class ArtistsController < ApplicationController
+  # show and index actions for both Song and Artist
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Artist.find_by(id: params[:id])
   end
 
   def new
