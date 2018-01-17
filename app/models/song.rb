6be43fb90a31701424651109f5eb6fb88585305d
johneckert <johnteckert@gmail.com>
2018-01-16 21:49:47 -0500

    done

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    self.artist.name
+  end
+
+  def title_name_formated
+    "#{self.artist_name} - #{self.title}"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+a1 = Artist.create(name: "Prince")
+a2 = Artist.create(name: "Bowie")
+a3 = Artist.create(name: "Zebra katz")
+a4 = Artist.create(name: "Childish Gambino")
+a5 = Artist.create(name: "NIN")
+
+s1 = Song.create(title: "Kiss", artist_id: 1)
+s2 = Song.create(title: "1999", artist_id: 1)
+s3 = Song.create(title: "Ziggy Stardust", artist_id: 2)
+s4 = Song.create(title: "Suffregette City", artist_id: 2)
+s5 = Song.create(title: "Pretty Things", artist_id: 2)
+s6 = Song.create(title: "Blk Wiccan", artist_id: 3)
+s7 = Song.create(title: "Ima Read", artist_id: 3)
+s8 = Song.create(title: "Firefly", artist_id: 4)
+s9 = Song.create(title: "Summer Camp", artist_id: 4)
+s10 = Song.create(title: "Head Like a Hole", artist_id: 5)
+s11 = Song.create(title: "Hurt", artist_id: 5)
